37ee6b93afec45d5bacdd5a13cded757e6827eb2
Furkan AK <furkan.ak@stu.fsm.edu.tr>
2024-08-17 16:45:16 +0300

    Update

--- src/bingogame/MainClass.java
+++ src/bingogame/MainClass.java
@@ -28,7 +28,7 @@ public class MainClass {
 
 		BingoGameClass game = new BingoGameClass(randomcard, randomcard, randomPermutation);
 
-		game.playGame(); // Play Gamee.
+		game.playGame(); // Play Game.
 
 	}
 
